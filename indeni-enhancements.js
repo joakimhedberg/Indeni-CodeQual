1f5f4e18fc5d25ed5b9ae1db58c7ca0b97e0b37d
Indeni_PJ <patrik.jonsson@gmail.com>
2017-12-01 14:57:04 +0800

    Updating to show the device id too

--- indeni-enhancements.js
+++ indeni-enhancements.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name Indeni expand alert info
-// @version 1.0
+// @name Indeni enhancements
+// @version 1.1
 // @description Make it easier to make alert screenshots
 // @match https://indeni.domain.local/*
 // @grant none
@@ -15,27 +15,32 @@ var globTamper = { alertInfoWidth: "" };
 
 setTimeout(function(){
 
-    $('main-nav').append("<div><button id=\"toggleExpandAlertInfo\" style=\"float:right;\">Expand alert summary</button></div>");
+    $("main-nav").append("<div><button id=\"toggleExpandAlertInfo\" style=\"float:right;\">Expand alert summary</button></div>");
 
     $("#toggleExpandAlertInfo").on("click", function(){
 
-        alertInfo = $("section.side-pane.open.visible");
+        var deviceName = $("tr.last td.device").text();
+        var alertInfo = $("section.side-pane.open.visible");
 
         if(globTamper.alertInfoWidth === ""){
 
             //Save the old width
             globTamper.alertInfoWidth = $(alertInfo).css("width");
 
+            //Save the old style in globTamper, increase the width of the alert details and show the device id
             $("section.list").toggle();
+            $("div.side-pane-content div.title div.content_wrap").prepend("<div id=\"alertDetailsDeviceName\" style=\"margin-bottom:20px;font-size:14px;font-weight:bold;\">Device Name: " + deviceName + "<br></div>");
             $(alertInfo).css("width", "1000px");
 
         } else {
 
+            //Restore the old style and remove the device title
             $(alertInfo).css("width", globTamper.alertInfoWidth);
+            $("#alertDetailsDeviceName").remove();
             $("section.list").toggle();
             globTamper.alertInfoWidth = "";
 
         }
     });
 
-}, 4000);
+}, 3000);
